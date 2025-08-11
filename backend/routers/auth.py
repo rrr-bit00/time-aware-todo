8bd964b2247aa2e8104bfc5bc3668fce81d779c5
rrr-bit00 <175315215+rrr-bit00@users.noreply.github.com>
2025-08-11 21:30:42 +0900

    feat/ログイン認証が成功した場合に、トークンを発行する処理を追加

--- backend/routers/auth.py
+++ backend/routers/auth.py
@@ -19,5 +19,8 @@ def login(user: UserLogin, db: Session = Depends(get_db)):
     # ユーザーが存在するか、パスワードが一致するか確認
     if not db_user or not verify_password(user.password,
         db_user.password):
-        raise HTTPException(status_code = '401',
+        raise HTTPException(status_code = 401,
             detail = "Emailかパスワードが違います")
+
+    access_token = create_access_token({"sub:" str(db_user.id)})
+    return {"access_token": access_token, "token_type": "bearer"}
