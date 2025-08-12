8ab2121dc55d227543e685077cae527c693e5631
rrr-bit00 <175315215+rrr-bit00@users.noreply.github.com>
2025-08-12 22:53:38 +0900

    feat: ペイロードで文字列型で取得したuser_idを整数型に変換しUser.idと比較するように修正

--- backend/core/deps.py
+++ backend/core/deps.py
@@ -25,7 +25,7 @@ def get_current_user(token: str = Depends(oauth2_schemas), db: Session = Depends
             raise credentials_exception
     except JWTError:
         raise credentials_exception
-    user = db.query(User).filter(User.id == user_id).first()
+    user = db.query(User).filter(User.id == int(user_id)).first()
     if user is None:
         raise credentials_exception
     return user
