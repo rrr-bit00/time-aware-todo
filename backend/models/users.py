5b45bc4df088a202a34a56d2ad90c567c282772e
rrr-bit00 <175315215+rrr-bit00@users.noreply.github.com>
2025-11-18 02:38:39 +0900

    WIP: user-schemas

--- backend/models/users.py
+++ backend/models/users.py
@@ -1,12 +1,33 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+import uuid
 
-Base = declarative_base()
+from datetime import datetime, func
+from sqlmodel import Field, DateTime, Relationship
 
-class User(Base):
-    __tablename__ = "users"
+from schemas.users import UserBase
+from models.tasks import Task
+from models.todos import Todo
+from models.screen_times import Screen_time
 
-    id = Column(Integer, primary_key = True, index = True)
-    username = Column(String, nullable = False)
-    email = Column(String, unique = True, index = True, nullable = False)
-    hash_pass = Column(String, nullable = False)
+# Base = declarative_base()
+
+
+class User(UserBase, table=True):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    hash_password: str
+    # 3つの子テーブルに対してリレーション
+    tasks: list["Task"] = Relationship(back_populates="user", cascade_delete=True)
+    todos: list["todo"] = Relationship(back_populates="user", cascade_delete=True)
+    screen_times: list["Screen_time"] = Relationship(
+        back_populates="user", cascade_delete=True
+    )
+
+    create_at: datetime = Field(
+        nullable=False,
+        sa_type=DateTime(timezone=True),
+        sa_column_kwargs={"onupdate": func.now(), "sever_default": func.now()},
+    )
+    update_at: datetime = Field(
+        nullable=False,
+        sa_type=DateTime(timezone=True),
+        sa_column_kwargs={"onupdate": func.now(), "sever_default": func.now()},
+    )
--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -1,13 +1,45 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import EmailStr
+from sqlmodel import Field, SQLModel
+
 
 # Usersテーブルの共通フィールドを定義
-class UserBase(BaseModel):
-    username: str
-    email: EmailStr
+class UserBase(SQLModel):
+    username: str | None = Field(default=None, index=True, max_length=255)
+    email: EmailStr = Field(unique=True, index=True, max_length=255)
+    is_active: bool = Field(default=True)
+    is_superuser = False
+
 
 # アカウント作成時のフィールドを定義
 class UserCreate(UserBase):
-    password: str
+    password: str = Field(min_length=8, max_length=20)
+
+
+# アカウント作成時のフィールドを定義（ユーザー）
+class UserRegister(SQLModel):
+    username: str | None = Field(default=None, max_length=255)
+    email: EmailStr = Field(max_length=255)
+    password: str = Field(min_length=8, max_length=29)
+
+
+# emailかpasswordの更新
+class UserUpdate(UserBase):
+    # 更新しない方を送る必要がないためNoneを許可
+    email: EmailStr | None = Field(default=None, max_length=255)
+    password: str | None = Field(default=None, min_length=8, max_length=20)
+
+
+# emailかpassowrdの更新（ユーザー）
+class UserUpdateMe(SQLModel):
+    username: str | None = Field(default=None, max_length=255)
+    email: EmailStr | None = Field(default=None, max_length=255)
+
+
+# passwordの更新（ユーザー）
+class PasswordUpdate(SQLModel):
+    current_password: str = Field(min_length=8, max_length=20)
+    new_password: str = Field(min_length=8, max_length=20)
+
 
 # 読み込む際の、フィールドを定義
 class UserRead(UserBase):
